2e2129ba72bbfd57416f77d44a67ff1572525e44
Niclas Svanström <niclas.svanstrom@hotmail.com>
2023-01-09 13:32:15 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, upgrade
+
+from model import db, seedData
+
+ 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:Nelsonpelson01@localhost/Bank'
+db.app = app
+db.init_app(app)
+migrate = Migrate(app,db)
+ 
+ 
+
+@app.route("/")
+def startpage():
+    return "Hej"
+    # trendingCategories = Category.query.all()
+    # return render_template("index.html", trendingCategories=trendingCategories)
+
+@app.route("/category/<id>")
+def category(id):
+    return "Hej2"
+    # products = Product.query.all()
+    # return render_template("category.html", products=products)
+
+
+if __name__  == "__main__":
+    with app.app_context():
+        upgrade()
+    
+        seedData(db)
+        app.run()
+
