7c4f0345114a3a015a7c97ef7408c4f8de269300
Niclas Svanström <niclas.svanstrom@hotmail.com>
2023-01-24 15:01:40 +0100

    the top 10 is working

--- app.py
+++ app.py
@@ -53,9 +53,33 @@ def startpage():
 
 @app.route("/country/<c>")
 def country(c):
+    class Personas():
+        def __init__(self, customer_id, money, name, lastname):
+            self.__customer_id = customer_id
+            self.__money = money
+            self.__name = name
+            self.__lastname = lastname
+        def get_id(self):
+            return self.__customer_id
+        def get_money(self):
+            return self.__money
+        def get_name(self):
+            return self.__name
+        def get_lastname(self):
+            return self.__lastname
     customers= Customer.query.filter_by(Country=c).all()
+    list_of_customers = []
+    for c in customers:
+        accounts = Account.query.filter_by(CustomerId=c.Id).all()
+        sum = 0
+        for a in accounts:
+            sum += a.Balance
+        persona = Personas(c.Id,sum,c.GivenName,c.Surname)
+        list_of_customers.append(persona)
+    list_of_customers.sort(reverse=True, key=lambda x: x.get_money())
+    list_of_customers = list_of_customers[:10]
     if 'loggedin' in session:
-        return render_template("country.html", customers=customers)
+        return render_template("country.html", customers=customers, list_of_customers=list_of_customers)
     else:
         return redirect(url_for('login'))
 
